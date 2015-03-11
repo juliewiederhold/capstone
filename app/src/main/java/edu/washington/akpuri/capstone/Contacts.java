f8bc6986e5232d60c83ab22baf36141ff60abf0c
Akash <akashkpuri@gmail.com>
2015-03-11 16:16:28 -0700

    Updted contacts page import button

--- app/src/main/java/edu/washington/akpuri/capstone/Contacts.java
+++ app/src/main/java/edu/washington/akpuri/capstone/Contacts.java
@@ -112,6 +112,7 @@ public class Contacts extends ActionBarActivity {
                                             }
                                         }
                                     });
+
                                 }
                                 Log.e("Contacts", "Should've saved contacts.");
                                 parseObject.addAllUnique("contacts", pendingParseContacts);
@@ -176,24 +177,73 @@ public class Contacts extends ActionBarActivity {
 
         @Override
         public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+            Log.i("ContactsFragment", "onCreateView Fired for ContactsFrgment");
             final View rootView = inflater.inflate(R.layout.fragment_contacts, container, false);
             pendingContacts = new ArrayList<Contact>();
             pendingParseContacts = new ArrayList<ParseObject>();
+            final SingletonContacts instance = SingletonContacts.getInstance();
 
             Button importContacts = (Button) rootView.findViewById(R.id.importContacts);
-            if (allowContactRetrieval) {
-                importContacts.setVisibility(View.GONE);
-            }
             importContacts.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
                     AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
                     builder.setMessage("Allow So-So to use your Contacts? It's awfully Risky you know")
                             .setTitle("Import Contacts");
+
                     builder.setPositiveButton("Allow", new DialogInterface.OnClickListener() {
                         public void onClick(DialogInterface dialog, int id) {
                             // User clicked OK button
+                            //Now we update the imported contact list
                             allowContactRetrieval = true;
+                            allContacts = new ArrayList<Contact>();
+                            //ContentResolver is used to query the contacts database to return a cursor
+                            ContentResolver contentResolver = rootView.getContext().getContentResolver();
+                            //The cursor is like an iterator, it contains the entirety of the contacts when we pass it null paramaters
+                            Cursor cur = contentResolver.query(ContactsContract.Contacts.CONTENT_URI, null, null, null, null);
+                            //Check to see if the cursor actually got contacts back
+                            Log.i("Querying Contacts", "Cur Count is :" + cur.getCount());
+                            if (cur.getCount() > 0) {
+                                while (cur.moveToNext()) {
+                                    //Use cursor to query, here we grab the current contacts ID which can be used to get
+                                    //More information for that particular contact later
+                                    int identity = Integer.parseInt(cur.getString(cur.getColumnIndex(ContactsContract.Contacts._ID)));
+                                    String name = cur.getString(cur.getColumnIndex(ContactsContract.Contacts.DISPLAY_NAME));
+                                    String phone = "";
+                                    //Check to see if contact has a phone number
+                                    if (Integer.parseInt(cur.getString(
+                                            cur.getColumnIndex(ContactsContract.Contacts.HAS_PHONE_NUMBER))) > 0) {
+                                        //If they do have a phone number, we need to create a new cursor for phone because
+                                        //Contact phone numbers are stored in a separate database table so must be queried separately
+                                        Cursor phoneCur = contentResolver.query(
+                                                ContactsContract.CommonDataKinds.Phone.CONTENT_URI,
+                                                null,
+                                                ContactsContract.CommonDataKinds.Phone.CONTACT_ID + " = ?",
+                                                new String[]{identity + ""}, null);
+                                        while (phoneCur.moveToNext()) {
+                                            int type = phoneCur.getInt(phoneCur.getColumnIndex(ContactsContract.CommonDataKinds.Phone.TYPE));
+                                            //We only want to grab the mobile phone number of a contact
+                                            if (type == ContactsContract.CommonDataKinds.Phone.TYPE_MOBILE) {
+                                                phone = phoneCur.getString(phoneCur.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER));
+                                            }
+                                        }
+                                        phoneCur.close();
+                                    }
+                                    //If a contact does not have a phone number stored or their phone number is shorter than
+                                    //a length of 7 (example: AT&T service numbers for things like checking data are only 4 characters long)
+                                    //
+                                    Contact person = new Contact(name, phone, identity);
+                                    if (phone.length() >= 7) {
+                                        allContacts.add(person);
+                                        Log.i("Contacts", "Contact: " + name + " has ID of " + identity + " and phone number of " + phone);
+                                    }
+                                }
+                                cur.close();
+                                instance.setContacts(allContacts);
+                                ListView contactListView = (ListView) rootView.findViewById(R.id.listView);
+                                ListAdapter adapter = new ContactAdapter(rootView.getContext(), R.id.contactListItem, allContacts, pendingContacts);
+                                contactListView.setAdapter(adapter);
+                            }
                         }
                     });
                     builder.setNegativeButton("Deny", new DialogInterface.OnClickListener() {
@@ -203,72 +253,13 @@ public class Contacts extends ActionBarActivity {
                         }
                     });
                     AlertDialog dialog = builder.create();
-                    dialog.show();
+                    if (!allowContactRetrieval) {
+                        dialog.show();
+                    }
                 }
             });
             //Popup dialog for contact retrieval
-            if(!allowContactRetrieval) {
-
-                //Store contacts in the Singleton Object Contacs
-                SingletonContacts instance = SingletonContacts.getInstance();
-                //If the Singleton already has a list on contacs, then there is no need to query all of them
-                //again
-                if (!instance.hasContacts()) {
-                    allContacts = new ArrayList<Contact>();
-                    //ContentResolver is used to query the contacts database to return a cursor
-                    ContentResolver contentResolver = rootView.getContext().getContentResolver();
-                    //The cursor is like an iterator, it contains the entirety of the contacts when we pass it null paramaters
-                    Cursor cur = contentResolver.query(ContactsContract.Contacts.CONTENT_URI, null, null, null, null);
-                    //Check to see if the cursor actually got contacts back
-                    Log.i("Querying Contacts", "Cur Count is :" + cur.getCount());
-                    if (cur.getCount() > 0) {
-                        while (cur.moveToNext()) {
-                            //Use cursor to query, here we grab the current contacts ID which can be used to get
-                            //More information for that particular contact later
-                            int id = Integer.parseInt(cur.getString(cur.getColumnIndex(ContactsContract.Contacts._ID)));
-                            String name = cur.getString(cur.getColumnIndex(ContactsContract.Contacts.DISPLAY_NAME));
-                            String phone = "";
-                            //Check to see if contact has a phone number
-                            if (Integer.parseInt(cur.getString(
-                                    cur.getColumnIndex(ContactsContract.Contacts.HAS_PHONE_NUMBER))) > 0) {
-                                //If they do have a phone number, we need to create a new cursor for phone because
-                                //Contact phone numbers are stored in a separate database table so must be queried separately
-                                Cursor phoneCur = contentResolver.query(
-                                        ContactsContract.CommonDataKinds.Phone.CONTENT_URI,
-                                        null,
-                                        ContactsContract.CommonDataKinds.Phone.CONTACT_ID + " = ?",
-                                        new String[]{id + ""}, null);
-                                while (phoneCur.moveToNext()) {
-                                    int type = phoneCur.getInt(phoneCur.getColumnIndex(ContactsContract.CommonDataKinds.Phone.TYPE));
-                                    //We only want to grab the mobile phone number of a contact
-                                    if (type == ContactsContract.CommonDataKinds.Phone.TYPE_MOBILE) {
-                                        phone = phoneCur.getString(phoneCur.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER));
-                                    }
-                                }
-                                phoneCur.close();
-                            }
-                            //If a contact does not have a phone number stored or their phone number is shorter than
-                            //a length of 7 (example: AT&T service numbers for things like checking data are only 4 characters long)
-                            //
-                            Contact person = new Contact(name, phone, id);
-                            if (phone.length() >= 7) {
-                                allContacts.add(person);
-                                Log.i("Contacts", "Contact: " + name + " has ID of " + id + " and phone number of " + phone);
-                            }
-                        }
-                        cur.close();
-                        instance.setContacts(allContacts);
-                    }
-                    //            contactListView.setOnItemClickListener(new OnItemClickListener() {
-                    //                public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                    //                    Contact person = (Contact) parent.getItemAtPosition(position);
-                    //                    Toast.makeText(getApplicationContext(),
-                    //                            "Clicked on Row: " + person.getName(),
-                    //                            Toast.LENGTH_LONG).show();
-                    //                }
-                    //            });
-
-                }
+            if(allowContactRetrieval) {
                 allContacts = instance.getContacts();
                 //We now have all relevant contacts stored in our ArrayList of Contacts with their name, and phone number
                 //Now we need to set up the array adapter
